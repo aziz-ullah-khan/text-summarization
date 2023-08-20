a45afd9a9735a444f0f41bfe4f09b6b127fa4a30
aziz-ullah-khan <azizullah444@gmail.com>
2023-08-20 15:04:53 +0500

    requirements & installation done

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, force='[%(asctime)]: %(message)s:')
 
-project_name = "text-summarization"
+project_name = "text-summarization"  #textSummarization will best next time
 
 list_of_files = [
     ".github/workflows/.gitkeep",
