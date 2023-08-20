6513ca229e2f5a2a6dd55db6cdded419f7aa9bdd
aziz-ullah-khan <azizullah444@gmail.com>
2023-08-20 23:29:54 +0500

    data ingestoin added

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from textSummarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarization.logging import logger
 
 
-
-logger.info("Welcome to Text Summerizer")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
